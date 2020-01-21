58273ed410d5e38bd037e61631321398c3be05e0
Dominik Pawlik <osemka59@gmail.com>
2020-01-21 19:31:27 +0100

    Added one more missing comment about program

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-var tab_znakow = new Array("Ą","ą", "Ę","ę", "Ł", "ł", "Ź", "ź", "Ż", "ż", "Ć", "ć", "Ś", "ś", "Ń", "ń", "Ó", "ó");
+/**
+ * 1. Używanie polskich znaków
+ * 2. Emulacja logiki boolowskiej za pomocą integerów
+ * 3. Znaczna komplikacja kodu
+ * 4. Bardzo długie linie które nie mieszczą się w 120 kolumnach
+ * 5. Kilka punktów wyjścia z programu
+ * 6. Używanie niebezpiecznych porównań == zamiast ===
+ * 7. Program nie działa jak powinien - zwraca 1(true) pomimo wpisania Gżegżółka123.-
+ */
+
+var tab_znak = new Array("Ą","ą", "Ę","ę", "Ł", "ł", "Ź", "ź", "Ż", "ż", "Ć", "ć", "Ś", "ś", "Ń", "ń", "Ó", "ó");
 
 function polska_litera(litera) {
     var NO = 0;
     var OK = 1;
 
-    var tab_dl = tab_znakow.length;
+    var tab_dlug = tab_znak.length;
 
-    for(j = 0; j <= tab_dl; j++) {
-        if(litera == tab_znakow[j] ) {
+    for(j = 0; j <= tab_dlug; j++) {
+        if(litera == tab_znak[j] ) {
             return OK;
         }
     }
@@ -15,15 +25,15 @@ function polska_litera(litera) {
     return NO;
 }
 
-function tylko_litery_i_cyfry(wyraz) {
+function tylko_cyfry_i_litery(wyraz) {
     var OK = 1
     var NO = 0
 
     wyraz = wyraz.toUpperCase();
 
-    var dlugosc = wyraz.length;
+    var rozmiar = wyraz.length;
 
-    for (i = 0; i < dlugosc; i++) {
+    for (i = 0; i < rozmiar; i++) {
         if (wyraz.charAt(i) < "A" && wyraz.charAt(i) != "-" && wyraz.charAt(i) != " " && wyraz.charAt(i) != "." && wyraz.charAt(i) != "&" && wyraz.charAt(i) != "_" && wyraz.charAt(i) != "0" && wyraz.charAt(i) != "1" && wyraz.charAt(i) != "2" && wyraz.charAt(i) != "3" && wyraz.charAt(i) != "4" && wyraz.charAt(i) != "4" && wyraz.charAt(i) != "5" && wyraz.charAt(i) != "7" && wyraz.charAt(i) != "8" && wyraz.charAt(i) != "9") {
             if (polska_litera(wyraz.charAt(i)) == 0)
                 return NO;
@@ -41,16 +51,21 @@ function tylko_litery_i_cyfry(wyraz) {
         }
     }
     var licznik = 0;
-    for (i = 0; i < dlugosc; i++) {
+    for (i = 0; i < rozmiar; i++) {
         if (wyraz.charAt(i) == "-" || wyraz.charAt(i) == " " || wyraz.charAt(i) == "." || wyraz.charAt(i) == "&" || wyraz.charAt(i) == "_") {
             licznik++;
         }
     }
-    if (dlugosc > 0)
-        if (licznik == dlugosc) {
+    if (rozmiar > 0)
+        if (licznik == rozmiar) {
             return NO;
         }
     return OK;
 }
 
-console.log(tylko_litery_i_cyfry('123ą'));
+console.log('123ą  ' + tylko_cyfry_i_litery('123ą'));
+console.log('Polska  ' + tylko_cyfry_i_litery('Polska'));
+console.log('Polska123  ' + tylko_cyfry_i_litery('Polska123'));
+console.log('Gżegżółka123  ' + tylko_cyfry_i_litery('Gżegżółka123'));
+console.log('Gżegżółka123.-  ' + tylko_cyfry_i_litery('Gżegżółka123.-'));
+console.log('Gżegżółka123.-"  ' + tylko_cyfry_i_litery('Gżegżółka123.-"'));
